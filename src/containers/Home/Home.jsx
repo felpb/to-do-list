38543601e877709672fa47422b0df682c0aa96d7
Felipe Bilibio <fallensky510@gmail.com>
2024-04-09 10:28:00 -0300

    feat: add tasks

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import './Home.css';
 
 function Home() {
+  const [tasks] = useState([
+    {
+      id: 1,
+      name: 'Estudar Javascript',
+      done: false,
+    },
+    {
+      id: 2,
+      name: 'Estudar Banco de dados',
+      done: false,
+    },
+    {
+      id: 3,
+      name: 'Programar em java',
+      done: true,
+    },
+  ]);
+
   return <div className="container">Start</div>;
 }
 
