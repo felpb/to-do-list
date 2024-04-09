1d724bf4db6eb16c1fd30154180d0dd8d87fa471
Felipe Bilibio <fallensky510@gmail.com>
2024-04-09 10:54:18 -0300

    style: styling tasks

--- src/containers/Task/Task.jsx
+++ src/containers/Task/Task.jsx
@@ -1,7 +1,21 @@
+import { IoMdCheckmark, IoMdTrash } from 'react-icons/io';
 import './Task.css';
 
 function Task({ task }) {
-  return <div>{task.name}</div>;
+  return (
+    <div className="task-container">
+      <div>{task.name}</div>
+
+      <div className="task-buttons">
+        <button className="check-button" type="button">
+          <IoMdCheckmark />
+        </button>
+        <button className="delete-button" type="button">
+          <IoMdTrash />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
